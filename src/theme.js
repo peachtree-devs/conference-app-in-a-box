a6e5b31843dbfac08724d4b15890617cab0db02a
Mone't Fulgham <monetfulgham@gmail.com>
2019-12-16 18:22:31 -0500

    Update color palette

--- src/theme.js
+++ src/theme.js
@@ -3,14 +3,14 @@ import { Platform, Dimensions } from 'react-native'
 const dimensions = Dimensions.get('window')
 const logo = require('./assets/logo.jpg')
 
-const primary = 'rgba(18, 25, 50, 1)'
-const primaryLight = 'rgba(27, 37, 77, 1)'
-const primaryDark = 'rgba(14, 20, 43, 1)'
-const primaryOpaque = opacity => `rgba(18, 25, 50, ${opacity})`
+const primary = '#000000';
+const primaryLight = '#62C1E9';
+const primaryDark = '#000000';
+const primaryOpaque = '#EFAA6F';
 
-const primaryText = 'white'
+const primaryText = 'white';
 
-const highlight = '#61dafb'
+const highlight = '#ffffff';
 
 const colors = {
   primary,
@@ -19,14 +19,14 @@ const colors = {
   primaryDark,
   primaryOpaque,
   primaryText
-}
+};
 
 const typography = {
   primary: "Gotham Rounded",
   secondary: "Gotham Rounded",
   medium: "GothamRnd Medium",
   bold: "Gotham Bold"
-}
+};
 
 
 export {
